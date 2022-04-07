584a8bbca2e2f89fbd82ff354d07dd8e278e5942
antoninaorobets <pani.antonina.don@gmail.com>
2022-04-07 17:01:03 -0400

    commit tree cc8361878294965a6858b856d099470b7bb70dd9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require "pry"
 def oxford_comma(array)
 
+case  array.length
+    when 1 then ret = array.join
+    when 2 then ret = array.join(" and ")
+    else ret = array[0...-1].join(", ") + ", and " + array[-1]
+    end
+ret
 end
+
